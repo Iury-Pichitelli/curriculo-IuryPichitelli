d9a4e579ece4336656b6336f03702aa2eadde1db
Iury <157545189+Iury-Pichitelli@users.noreply.github.com>
2025-06-15 22:52:25 -0300

    fixing aside hide

--- script/script.js
+++ script/script.js
@@ -2,9 +2,5 @@ var btn_menu = document.querySelector('#btn_menu');
 var menu_hide = document.querySelector('aside');
 
 btn_menu.addEventListener('click', function(){
-    if(menu_hide.style.display === 'none'){
-        menu_hide.style.display = 'block';
-    } else{
-        menu_hide.style.display = 'block';
-    }
+    menu_hide.classList.toggle('show');
 });
