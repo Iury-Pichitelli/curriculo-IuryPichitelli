03ecf8d989051a7e23103ddd27e2713a6ae0e92a
Iury <157545189+Iury-Pichitelli@users.noreply.github.com>
2025-06-15 23:48:31 -0300

    updating

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 var btn_menu = document.querySelector('#btn_menu');
 var menu_hide = document.querySelector('aside');
+var img_hide = document.querySelector('#image_short_profile')
 
 btn_menu.addEventListener('click', function(){
     menu_hide.classList.toggle('show');
+    img_hide.classList.toggle('show_img');
 });
