c8db10fb00c790cae8c2a9f7ce28d67d953ffbf2
Iury <157545189+Iury-Pichitelli@users.noreply.github.com>
2025-06-15 23:59:16 -0300

    modifications

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 var btn_menu = document.querySelector('#btn_menu');
+
 var menu_hide = document.querySelector('aside');
 var img_hide = document.querySelector('#image_short_profile')
+var bloco_1 = document.querySelector('.bloco_1');
+var bloco_2 = document.querySelector('.bloco_2');
+var bloco_3 = document.querySelector('.bloco_3');
 
 btn_menu.addEventListener('click', function(){
     menu_hide.classList.toggle('show');
-    img_hide.classList.toggle('show_img');
+    img_hide.classList.toggle('show_elements_aside');
+    bloco_1.classList.toggle('show_elements_aside');
+    bloco_2.classList.toggle('show_elements_aside');
+    bloco_3.classList.toggle('show_elements_aside');
 });
